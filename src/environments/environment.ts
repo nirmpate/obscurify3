91f487ec6149d22ec6e023ddab049ea9ee8bf54a
nirmpate <npatel663@gmail.com>
2020-06-01 22:38:02 -0500

    update obscurify rating

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   redirect_uri: 'https://ktp0b5os1g.execute-api.us-east-2.amazonaws.com/dev/callbackLocal',
-  obscurifyApiUrl: '',
+  obscurifyApiUrl: 'https://ktp0b5os1g.execute-api.us-east-2.amazonaws.com/dev',
   client_id: '31e362f6085c4309a2e1a6d2c0f8d8ec'
 };
 
