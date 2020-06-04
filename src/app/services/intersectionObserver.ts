a45f5cec059c2c4b72269d36902df3ed51da9f3e
nirmpate <npatel663@gmail.com>
2020-06-03 20:58:18 -0500

    upgrade to 9

--- src/app/services/intersectionObserver.ts
+++ src/app/services/intersectionObserver.ts
@@ -1,5 +1,7 @@
 import { Subject } from 'rxjs';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class IntersectionObserverService {
     private intersectionObserverSubject = new Subject<any>();
 
