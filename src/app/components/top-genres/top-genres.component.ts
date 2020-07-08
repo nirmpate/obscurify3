212f00dcea5bb69ba0888745092dcfb3aae46dc8
nirmal <npatel663@gmail.com>
2020-07-07 19:45:36 -0500

    update threshold in genres

--- src/app/components/top-genres/top-genres.component.ts
+++ src/app/components/top-genres/top-genres.component.ts
@@ -56,7 +56,7 @@ export class TopGenresComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     this.intersectionObserverService.init(this.element.nativeElement, {
-      threshold: [0.70]
+      threshold: [0.20]
     });
     this.intersectionObserverSubs = this.intersectionObserverService
     .getSubject()
