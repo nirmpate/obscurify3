c886c9bd2e3d7bdddc4f0d88f08de02b40106ee9
Alex Olivero <alex@Alexs-MacBook-Pro.local>
2020-09-23 11:47:02 -0500

    try to make twitter card image work

--- src/app/services/metaData.ts
+++ src/app/services/metaData.ts
@@ -28,11 +28,11 @@ export class SeoService {
     this.meta.updateTag({ name: 'Description', content: config.description });
 
     // Twitter
-    this.meta.updateTag({ name: 'twitter:card', content: 'summary_large_image' });
-    this.meta.updateTag({ name: 'twitter:site', content: `@obscurifymusic` });
-    this.meta.updateTag({ name: 'twitter:title', content: config.title });
-    this.meta.updateTag({ name: 'twitter:description', content: config.description });
-    this.meta.updateTag({ name: 'twitter:image', content: config.image });
-    this.meta.updateTag({name: 'og:image', content: config.image });
+    // this.meta.updateTag({ name: 'twitter:card', content: 'summary_large_image' });
+    // this.meta.updateTag({ name: 'twitter:site', content: `@obscurifymusic` });
+    // this.meta.updateTag({ name: 'twitter:title', content: config.title });
+    // this.meta.updateTag({ name: 'twitter:description', content: config.description });
+    // this.meta.updateTag({ name: 'twitter:image', content: config.image });
+    // this.meta.updateTag({name: 'og:image', content: config.image });
   }
 }
