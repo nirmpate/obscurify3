4873e6c2a2d72290b3cea45ef8478e763804c35b
nirmpate <npatel663@gmail.com>
2020-10-12 23:34:39 -0500

    WIP Create article module

--- src/app/articles/articles.module.ts
+++ src/app/articles/articles.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { SpotlightComponent } from './spotlight/spotlight.component';
 import { BigCardComponent } from './big-card/big-card.component';
 import { SmallCardComponent } from './small-card/small-card.component';
+import { FlexLayoutModule, FlexModule } from '@angular/flex-layout';
 
 
 
 @NgModule({
   declarations: [SpotlightComponent, BigCardComponent, SmallCardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FlexModule,
+    FlexLayoutModule
   ],
   exports: [SpotlightComponent, BigCardComponent, SmallCardComponent]
 })
