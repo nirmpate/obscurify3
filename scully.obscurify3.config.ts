c867e2df4dfef87ccb866a11955ed13ed08c0cd1
Alex Olivero <oliveroalex@gmail.com>
2021-01-15 00:33:07 -0600

    ran scully install

---
+++ scully.obscurify3.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "obscurify3",
+  outDir: './dist/static',
+  routes: {
+  }
+};
