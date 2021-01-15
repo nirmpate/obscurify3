8e66bbd307c61d7dc254c4a8fb5a83a368c5ac03
Alex Olivero <oliveroalex@gmail.com>
2021-01-15 00:47:41 -0600

    fixed scully config

--- scully.obscurify3.config.ts
+++ scully.obscurify3.config.ts
@@ -4,5 +4,11 @@ export const config: ScullyConfig = {
   projectName: "obscurify3",
   outDir: './dist/static',
   routes: {
+    '/blog/:slug': {
+      type: 'contentFolder',
+      slug: {
+        folder: "./blog"
+      }
+    },
   }
 };
