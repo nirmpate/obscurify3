1773a64ffb7a282a1e7c5939f5cace77a63fba3f
nirmpate <npatel663@gmail.com>
2021-02-16 21:39:50 -0600

    create column and hrefs

--- src/app/decade-card/decade-card.component.ts
+++ src/app/decade-card/decade-card.component.ts
@@ -1,23 +1,24 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-decade-card',
   templateUrl: './decade-card.component.html',
   styleUrls: ['./decade-card.component.scss']
 })
-export class DecadeCardComponent implements OnInit {
+export class DecadeCardComponent implements OnInit, AfterViewInit {
 
   @Input() decadeConfig;
   @ViewChild('trackGroup') trackGroup;
-  constructor() { }
+
+  constructor(public sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
 
   }
 
   ngAfterViewInit(): void {
-    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
-    //Add 'implements AfterViewInit' to the class.
+
     if (this.decadeConfig.current.length > 0) {
       this.trackGroup.value = 'current';
     } else {
