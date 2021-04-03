08005a38429b9040cc88e650f41864daa03f491e
Alex Olivero <oliveroalex@gmail.com>
2021-04-03 11:07:35 -0400

    reduced max decade track count, sort decade tracks by year

--- src/app/decade-card/decade-card.component.ts
+++ src/app/decade-card/decade-card.component.ts
@@ -15,7 +15,7 @@ export class DecadeCardComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
     console.log(this.decadeConfig)
     this.decadeConfig.combinedList = [];
-    while (this.decadeConfig.combinedList.length <= 8 && (this.decadeConfig.current.length > 0 || this.decadeConfig.allTime.length > 0)) {
+    while (this.decadeConfig.combinedList.length <= 6 && (this.decadeConfig.current.length > 0 || this.decadeConfig.allTime.length > 0)) {
       if (this.decadeConfig.current.length > 0) {
         this.decadeConfig.combinedList.push(this.decadeConfig.current.shift());
       }
@@ -23,6 +23,9 @@ export class DecadeCardComponent implements OnInit, AfterViewInit {
         this.decadeConfig.combinedList.push(this.decadeConfig.allTime.shift());
       }
     }
+    this.decadeConfig.combinedList = this.decadeConfig.combinedList.sort((a, b) => {
+      return parseInt(a.album.release_date.substring(0, 4), 10) - parseInt(b.album.release_date.substring(0, 4), 10);
+    })
   }
 
   ngAfterViewInit(): void {
