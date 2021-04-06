402cf84cd07115dc0a3e9220d75eb0c5459612ae
Alex Olivero <oliveroalex@gmail.com>
2021-04-06 14:13:35 -0500

    fixed spotiparel image and url

--- src/app/components/sponsored/sponsored.component.ts
+++ src/app/components/sponsored/sponsored.component.ts
@@ -8,9 +8,12 @@ import { Component, Input, OnInit } from '@angular/core';
 export class SponsoredComponent implements OnInit {
   @Input() allTimeTracks;
 
+  private href: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.href = `https://spotiparel.com?utm_source=obscurify&utm_medium=banner&ref_tracks=${this.allTimeTracks.slice(0,6).map(track => track.id).join()}`
   }
 
 }
