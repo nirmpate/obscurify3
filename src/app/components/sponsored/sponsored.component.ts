932e54d6895370cc3fe51dbcb0401deebed972af
Alex Olivero <oliveroalex@gmail.com>
2021-04-07 14:38:37 -0500

    filter out long track names

--- src/app/components/sponsored/sponsored.component.ts
+++ src/app/components/sponsored/sponsored.component.ts
@@ -12,13 +12,19 @@ export class SponsoredComponent implements OnInit {
   private timer: any;
   private trackName: string;
   private trackNameIndex: number = 0;
+  private trackTitlesToDisplay: any[];
 
   constructor() { }
 
   ngOnInit(): void {
     this.href = `https://spotiparel.com?utm_source=obscurify&utm_medium=banner&ref_tracks=${this.allTimeTracks.slice(0,6).map(track => track.id).join()}`;
-    this.timer = setInterval(() => {this.onInterval()}, 7000 );
-    this.trackName = this.allTimeTracks[0].name;
+    this.timer = setInterval(() => {this.onInterval()}, 5000);
+    this.trackTitlesToDisplay = this.allTimeTracks.map(track => track.name).filter(track => track.length < 20);
+    console.log(this.trackTitlesToDisplay)
+    if (this.trackTitlesToDisplay.length == 0) {
+      this.trackTitlesToDisplay = this.allTimeTracks.map(track => track.name);
+    }
+    this.trackName = this.trackTitlesToDisplay[0];
   }
 
   ngOnDestroy(): void {
@@ -26,12 +32,13 @@ export class SponsoredComponent implements OnInit {
   }
 
   onInterval() {
-    if (this.trackNameIndex < 4 && this.trackNameIndex < this.allTimeTracks.length - 1) {
+    if (this.trackNameIndex < 20 && this.trackNameIndex < this.trackTitlesToDisplay.length - 1) {
       this.trackNameIndex += 1;
     } else {
       this.trackNameIndex = 0;
     }
-    this.trackName = this.allTimeTracks[this.trackNameIndex].name;
+    this.trackName = this.trackTitlesToDisplay[this.trackNameIndex];
+
   }
   
 
